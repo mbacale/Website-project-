e49a1f8aa86941c1622142b879bb7748d513555c
diegodilena <101748237+diegodilena@users.noreply.github.com>
2022-09-20 08:09:09 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function copyText() {
+      
+    /* Select text area by id*/
+    var Text = document.getElementById("textbox");
+
+    /* Copy selected text into clipboard */
+    navigator.clipboard.writeText(Text.value);
+
+    /* Set the copied text as text for 
+    div with id clipboard */
+    document.getElementById("clipboard")
+        .innerHTML = Text.value;
+}
